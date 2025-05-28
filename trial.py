8ac46d014e760ff7e736614c018b4f2e8e0688bb
Vismaya-2608 <mdvismaya2002@gmail.com>
2025-05-28 15:26:59 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -99,7 +99,7 @@ if sidebar_option == "Data Preview":
             </style>
         """, unsafe_allow_html=True)
 
-        col1, col2 = st.columns(2)
+        col1, col2 = st.columns(3)
 
         with col1:
             st.markdown("""
