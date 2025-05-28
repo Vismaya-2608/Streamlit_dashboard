ef9e55a9282d29311d43696fca625f2a8be57a71
Vismaya-2608 <mdvismaya2002@gmail.com>
2025-05-28 13:13:06 +0530

    trial.py

--- trial.py
+++ trial.py
@@ -13,18 +13,6 @@ st.set_page_config(layout="wide")
 st.title("🔍 Dubai Real Estate Dashboard")
 
 
-# Sample DataFrame (you can replace this with your own dataset)
-df = pd.DataFrame("sample_df.csv")
-
-st.title("Dataset Overview")
-
-# Button to display total records and columns
-if st.button("Show Dataset Info"):
-    total_records = df.shape[0]
-    total_columns = df.shape[1]
-    
-    st.success(f"Total Records: {total_records}")
-    st.success(f"Total Columns: {total_columns}")
 
 
 # --- File Paths ---
