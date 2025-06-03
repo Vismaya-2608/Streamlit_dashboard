d316e2a8a1cb7a33bdf3e3dfc81118809c9084e6
Vismaya-2608 <mdvismaya2002@gmail.com>
2025-06-03 17:04:08 +0530

    Updated chart trial.py

--- trial.py
+++ trial.py
@@ -114,12 +114,10 @@ elif sidebar_option == "Pareto Analysis":
         
 
     with tab2:
-        if os.path.exists(html_pereto_df):
-            with open(html_pereto_df, "r", encoding="utf-8") as f:
-                dt_html = f.read()
-            components.html(dt_html, height=500, width=3500, scrolling=False)
-        else:
-            st.error("Pareto analysis HTML file not found.")
+        fig.write_html("html_pereto_df", full_html=False, include_plotlyjs='cdn')
+        components.html(dt_html, height=600, scrolling=False)
+
+
 
     with tab3:
             
