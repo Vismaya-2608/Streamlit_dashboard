860a64c6e23ea07984254646f5f98959839b9f43
Vismaya-2608 <mdvismaya2002@gmail.com>
2025-06-05 12:10:56 +0530

    added geo graphical analysis

--- trial.py
+++ trial.py
@@ -409,7 +409,7 @@ if sidebar_option == "Bivariate Analysis":
         if box_plot:
             st.plotly_chart(box_plot, use_container_width=True)
 
-# --- View 1: Price Prediction Model ---
+# --- View 5: Price Prediction Model ---
 # Define file paths
 EXCEL_PATH = "All_model_output.xlsx"
 model_perfomance =  "Model_performance.xlsx"
@@ -543,6 +543,157 @@ if sidebar_option == "Price Prediction Model":
                         st.dataframe(df, use_container_width=True)
             else:
                 st.error(f"Excel file not found at: {EXCEL_PATH}")
+
+# --- View 6: Geo Graphical Analysis ---
+if sidebar_option == "Geo Graphical Analysis":
+    st.subheader("Dubai Area-wise Bubble Map")
+    df_excel = pd.read_excel("new_tdf.xlsx")
+    units_excel = pd.read_excel("units_20.xlsx")
+    # Create first bubble map 
+    Count_tab, Avg_tab = st.tabs(["nRecords","Average Meter sale price"])
+    with Count_tab:
+        figs = px.scatter_mapbox(
+            df_excel,
+            lat='area_lat',
+            lon='area_lon',
+            size='Average Meter Sale Price',
+            color='Transaction Count',
+            hover_name='area_name_en',
+            hover_data={
+                'Transaction Count': True,
+                'Average Meter Sale Price': ':.2f',
+                'area_lat': False,
+                'area_lon': False
+            },
+            color_continuous_scale='Viridis',
+            size_max=30,
+            zoom=9,
+            title="Dubai Area-wise Average Meter Sale Price and Transaction Count"
+        )
+
+        for trace in figs.data:
+            trace.name = "Raw data"
+            trace.legendgroup = "Raw data"
+            trace.showlegend = True
+    
+        # Create second bubble map
+        fig2 = px.scatter_mapbox(
+            units_excel,
+            lat='area_lat',
+            lon='area_lon',
+            size='Average Meter Sale Price',
+            color='Transaction Count',
+            hover_name='area_name_en',
+            hover_data={
+                'Transaction Count': True,
+                'Average Meter Sale Price': ':.2f',
+                'area_lat': False,
+                'area_lon': False
+            },
+            color_continuous_scale='Viridis',
+            size_max=30,
+            opacity=0.6,
+            zoom=9,
+            title="Dubai Area-wise Average Meter Sale Price and Transaction Count"
+        )
+
+        for trace in fig2.data:
+            trace.name = "Data >= 2020"
+            trace.legendgroup = "Data >= 2020"
+            trace.showlegend = True
+
+        # Combine the two maps
+        for trace in fig2.data:
+            figs.add_trace(trace)
+
+        figs.update_layout(
+            mapbox_style='open-street-map',
+            margin={"r": 0, "t": 40, "l": 0, "b": 0},
+            legend=dict(
+                x=0.01,
+                y=0.99,
+                bgcolor='rgba(255,255,255,0.8)',
+                bordercolor='black',
+                borderwidth=1
+            )
+        )
+
+        # Display the map
+        st.plotly_chart(figs, use_container_width=True)
+
+    
+    with Avg_tab:
+        figs = px.scatter_mapbox(
+            df_excel,
+            lat='area_lat',
+            lon='area_lon',
+            size='Transaction Count',
+            color='Average Meter Sale Price',
+            hover_name='area_name_en',
+            hover_data={
+                'Transaction Count': True,
+                'Average Meter Sale Price': ':.2f',
+                'area_lat': False,
+                'area_lon': False
+            },
+            color_continuous_scale='Viridis',
+            size_max=30,
+            zoom=9,
+            title="Dubai Area-wise Average Meter Sale Price and Transaction Count"
+        )
+
+        for trace in figs.data:
+            trace.name = "Raw data"
+            trace.legendgroup = "Raw data"
+            trace.showlegend = True
+    
+        # Create second bubble map
+        fig2 = px.scatter_mapbox(
+            units_excel,
+            lat='area_lat',
+            lon='area_lon',
+            size='Transaction Count',
+            color='Average Meter Sale Price',
+            hover_name='area_name_en',
+            hover_data={
+                'Transaction Count': True,
+                'Average Meter Sale Price': ':.2f',
+                'area_lat': False,
+                'area_lon': False
+            },
+            color_continuous_scale='Viridis',
+            size_max=30,
+            opacity=0.6,
+            zoom=9,
+            title="Dubai Area-wise Average Meter Sale Price and Transaction Count"
+        )
+
+        for trace in fig2.data:
+            trace.name = "Data >= 2020"
+            trace.legendgroup = "Data >= 2020"
+            trace.showlegend = True
+
+        # Combine the two maps
+        for trace in fig2.data:
+            figs.add_trace(trace)
+
+        figs.update_layout(
+            mapbox_style='open-street-map',
+            margin={"r": 0, "t": 40, "l": 0, "b": 0},
+            legend=dict(
+                x=0.01,
+                y=0.99,
+                bgcolor='rgba(255,255,255,0.8)',
+                bordercolor='black',
+                borderwidth=1
+            )
+        )
+
+        # Display the map
+        st.plotly_chart(figs, use_container_width=True)
+
+
+
     
 
 
