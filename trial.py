3ba0448321f497d98d388219fc1af93d4f2bfea1
Vismaya-2608 <mdvismaya2002@gmail.com>
2025-06-06 17:49:33 +0530

    fixed legends

--- trial.py
+++ trial.py
@@ -441,7 +441,7 @@ if sidebar_option == "Bivariate Analysis":
                 xaxis_title=category_col,
                 yaxis=dict(title='nRecords'),
                 yaxis2=dict(title='Average Meter Sale Price', overlaying='y', side='right'),
-                legend=dict(x=1.1, y=1.0),
+                legend=dict(x=0.1, y=1.1),
                 hovermode='x unified',
                 barmode='group'
             )
