c811fe152a17833a8ce32a2fbf9013a946ba23fc
Vismaya-2608 <mdvismaya2002@gmail.com>
2025-06-12 17:59:36 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -381,7 +381,7 @@ if sidebar_option == "Univariate Analysis":
                     # Optional: customize layout
                     fig.update_layout(
                         #xaxis_title="meter_sale_price",
-                        yaxis_title="Number of Records",
+                        #yaxis_title="Number of Records",
                         bargap=0,  # Adjust space between bars
                         height=500
                         )
