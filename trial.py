c2921cbcccaed08e908254a5ec863ac0ae75c239
Vismaya-2608 <mdvismaya2002@gmail.com>
2025-06-23 12:01:58 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -75,8 +75,6 @@ sidebar_option = st.sidebar.radio("Choose View", [
     "Price Prediction Model"
 ])
 
-st.write(f"You selected: {sidebar_option}")
-
 # --- View 1: Data Summary ---
 if sidebar_option == "Data Summary":
     st.subheader("📄 Transactions Data")
